d9b0e182304d4fa097f1c2baef154f96ef00d414
Bedo <101138207+BeqaGogberashvili@users.noreply.github.com>
2022-07-03 12:34:23 +0400

    Navigate between pages
    Added some javascript and made buttons functional. I also styled inputs and organized HTML little bit.

--- script/app.js
+++ script/app.js
@@ -1,4 +1,88 @@
 
 const contentLeft = document.querySelector(".content-left")
 
-contentLeft.style.background = 'url("../img/unsplash_27LH_0jXKYI.png")';
+
+
+
+
+
+
+const steps = document.querySelectorAll(".step")
+
+const nextButtons = document.querySelectorAll(".next")
+const backButtons = document.querySelectorAll(".back")
+
+
+let currentStep = 0;
+displayCurrentStep(currentStep);
+
+
+
+
+function displayCurrentStep(currentStep) {
+    // Removing active class from any other step
+    steps.forEach(step => {
+        step.style.display = "none"
+    });
+    // Adding to current one
+    steps[currentStep].style.display = "block"
+}
+
+
+
+// Next button
+
+nextButtons.forEach((button, index) => {
+    button.addEventListener('click', (e) => {
+        // Preventing form from submitting
+        e.preventDefault()
+
+        switch (index) {
+
+            case 0:
+                currentStep++
+                break;
+
+            case 1:
+                currentStep++
+                break;
+        }
+
+        displayCurrentStep(currentStep)
+    })
+})
+
+
+backButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        currentStep--
+        displayCurrentStep(currentStep)
+    })
+})
+
+
+
+
+
+
+
+// window.onclick = function (event) {
+
+
+//     if (event.target === firstInput) {
+
+//         window.onclick = () => {
+
+//             if (firstInput.value === "") {
+//                 error.style.display = "block";
+//             } else {
+//                 error.style.display = "none";
+//             }
+
+//         }
+//     }
+
+// }
+
+
